404ff89019ea9adf446ffd43893c93bbfdef7ee9
Joanna <125133969+JoanKr@users.noreply.github.com>
2024-06-04 15:45:59 +0200

    Update rsa_signature.py

--- rsa_signature.py
+++ rsa_signature.py
@@ -1,34 +1,42 @@
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import hashes
-import numpy as np
+from cryptography.hazmat.primitives import hashes, serialization
 
-# Wczytaj dane z pliku
-quantized_values = np.load('quantized_values.npy')
-
-# Generowanie kluczy RSA (pierwsza para)
-private_key_1 = rsa.generate_private_key(
+# Generowanie kluczy RSA
+private_key = rsa.generate_private_key(
     public_exponent=65537,
     key_size=2048
 )
-public_key_1 = private_key_1.public_key()
+public_key = private_key.public_key()
 
-# Generowanie kluczy RSA (druga para, do testów)
-private_key_2 = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048
-)
-public_key_2 = private_key_2.public_key()
+# Zapisanie kluczy do plików
+with open("private_key.pem", "wb") as f:
+    f.write(private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    ))
 
-# Wiadomość do podpisania
-message = "To jest przykładowa wiadomość do podpisania.".encode('utf-8')
+with open("public_key.pem", "wb") as f:
+    f.write(public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    ))
+
+# Wczytanie wiadomości z pliku tekstowego
+with open("message.txt", "rb") as f:
+    message = f.read()
 
 # Wyliczenie skrótu wiadomości
 digest = hashes.Hash(hashes.SHA3_256())
 digest.update(message)
 message_hash = digest.finalize()
 
+# Zapisanie skrótu do pliku
+with open("message_hash.txt", "wb") as f:
+    f.write(message_hash)
+
 # Szyfrowanie skrótu kluczem prywatnym (tworzenie podpisu cyfrowego)
-signature = private_key_1.sign(
+signature = private_key.sign(
     message_hash,
     padding.PSS(
         mgf=padding.MGF1(hashes.SHA256()),
@@ -37,36 +45,6 @@ signature = private_key_1.sign(
     hashes.SHA256()
 )
 
-# Funkcja weryfikująca podpis
-def verify_signature(message, signature, public_key):
-    # Wyliczenie skrótu wiadomości
-    digest = hashes.Hash(hashes.SHA3_256())
-    digest.update(message)
-    message_hash = digest.finalize()
-
-    try:
-        public_key.verify(
-            signature,
-            message_hash,
-            padding.PSS(
-                mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=padding.PSS.MAX_LENGTH
-            ),
-            hashes.SHA256()
-        )
-        print("Podpis jest prawidłowy.")
-    except:
-        print("Podpis jest nieprawidłowy.")
-
-# Weryfikacja podpisu poprawnym kluczem publicznym
-print("Weryfikacja poprawnym kluczem publicznym:")
-verify_signature(message, signature, public_key_1)
-
-# Weryfikacja podpisu innym kluczem publicznym
-print("\nWeryfikacja innym kluczem publicznym:")
-verify_signature(message, signature, public_key_2)
-
-# Weryfikacja podpisu poprawnym kluczem publicznym ale inną wiadomością
-print("\nWeryfikacja poprawnym kluczem publicznym, ale inną wiadomością:")
-different_message = "To jest inna wiadomość.".encode('utf-8')
-verify_signature(different_message, signature, public_key_1)
+# Zapisanie podpisu do pliku
+with open("signature.sig", "wb") as f:
+    f.write(signature)
