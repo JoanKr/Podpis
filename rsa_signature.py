3f4aef61bd7c5fffe2e3886d896a075a83f71fb4
Joanna <125133969+JoanKr@users.noreply.github.com>
2024-06-10 23:31:51 +0200

    Update rsa_signature.py

--- rsa_signature.py
+++ rsa_signature.py
@@ -1,12 +1,4 @@
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import hashes, serialization
-
-# Generowanie kluczy RSA
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048
-)
-public_key = private_key.public_key()import numpy as np
+import numpy as np
 import secrets
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import hashes, serialization
